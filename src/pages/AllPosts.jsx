af29f8380e8c75e7b97a7027a4fb306a44f16294
vk880852 <115182567+vk880852@users.noreply.github.com>
2024-01-10 18:45:49 +0530

    Update AllPosts.jsx

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,27 +1,26 @@
-import React ,{useState,useEffect}from 'react'
-import appwriteService from '../appwrite/config'
-import { Container,PostCard } from '../Components'
+import React, {useState, useEffect} from 'react'
+import { Container, PostCard } from '../Components'
+import appwriteService from "../appwrite/config";
 
-const AllPosts = () => {
-    const [posts,setPosts]=useState([]);
-    useEffect(()=>{},[])
-    appwriteService.getPosts([]).then((posts)=>{
-        if(posts)setPosts(posts);
-    })
+function AllPosts() {
+    const [posts, setPosts] = useState([])
+    useEffect(() => { appwriteService.getPosts([]).then((posts) => {
+      if (posts) {
+          setPosts(posts.documents)
+      }
+  })}, [])
+   
   return (
     <div className='w-full py-8'>
-    <Container>
-       <div className='flex flex-wrap'>
-              {
-                posts.map((post)=>(
+        <Container>
+            <div className='flex flex-wrap'>
+                {posts.map((post) => (
                     <div key={post.$id} className='p-2 w-1/4'>
-                        <PostCard post={...post}/>
+                        <PostCard {...post} />
                     </div>
-                ))
-              }
-       </div>
-    </Container>
-      
+                ))}
+            </div>
+            </Container>
     </div>
   )
 }
