883cffe13e6341567c3ab696836d4457f7aa8afa
vk880852 <115182567+vk880852@users.noreply.github.com>
2024-01-10 19:37:32 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,64 @@
 import React, { useState, useEffect } from 'react';
 import appwriteService from '../appwrite/config';
 import { Container, PostCard } from '../Components';
+import { useSelector } from 'react-redux';
 
 const Home = () => {
+  const login = useSelector((state) => state.auth.status);
   const [posts, setPosts] = useState([]);
-  useEffect(()=>{
-    appwriteService.getPost().then((posts)=>{
-        if(posts)setPosts(posts.documents)
-    })
-  },[]);
-   if(posts.length==0)
-   {
-     return (<div className='w-full py-8 mt-4 text-center'>
-        <Container>
-            <div className='flex flex-wrap'>
-            <div className='p-2 w-full'>
-              <h1 className='text-2xl font-bold hover:text-gray-500'>
-                Login To Read Posts
-              </h1>
-            </div>
-        
-            </div>
-        </Container>
-     </div>)
-   }
-   else 
-   {
-      <div className='w-full py-8'>
-      <Container>
-        <div className='flex flex-wrap'>
-        {
-           posts.map((posts)=>(<div key={posts.$id} className='p-2 w-1/4'>
-            <PostCard {...posts}/>
-           </div>))
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await appwriteService.getPosts();
+        if (response.documents) {
+          setPosts(response.documents);
         }
-        </div>
-      </Container>
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    };
+
+    fetchPosts();
+  }, []); // Empty dependency array to run the effect only once on mount
+
+  if (posts.length === 0) {
+    return (
+      <div className='w-full py-8 mt-4 text-center'>
+        <Container>
+          <div className='flex flex-wrap'>
+            <div className='p-2 w-full'>
+              {!login ? (
+                <h1 className='text-2xl font-bold hover:text-gray-500'>
+                  Login To Read The Page
+                </h1>
+              ) : (
+                <h1 className='text-2xl font-bold hover:text-gray-500'>
+                  Welcome To Home Page
+                </h1>
+              )}
+            </div>
+          </div>
+        </Container>
       </div>
-   }
-  
+    );
+  } else {
+    return (
+      
+        <div className='w-full py-8'>
+          <Container>
+            <div className='flex flex-wrap'>
+              {posts.map((post) => (
+                <div key={post.$id} className='p-2 w-1/4'>
+                  <PostCard {...post} />
+                </div>
+              ))}
+            </div>
+          </Container>
+        </div>
+     
+    );
+  }
 };
 
 export default Home;
